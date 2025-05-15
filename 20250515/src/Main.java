fc1909d4542f46935a5d7238d6a3b4c0de589ae5
Jeong Mo Shin <shin751025@gmail.com>
2025-05-15 18:46:50 +0900

    Add files via upload

---
+++ 20250515/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = 0;
+        for (int n = 1; n <= 100; n++) {
+            System.out.println("1부터 100사이의 숫자를 입력하세요.");
+            num = sc.nextInt();
+            if (1 <= num && num <= 100) {
+                System.out.println("입력 완료! 프로그램을 종료합니다.");
+                break;
+            }
+        }
+    }
+}
